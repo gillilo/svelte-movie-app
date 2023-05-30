2359a556e2d49b6f8481969460ffbc7ff3bcfe18
gillilo <ghlwns9258@gmail.com>
2023-05-31 02:56:24 +0900

    Add Netlify functions

--- functions/test/test.js
+++ functions/test/test.js
@@ -1,17 +1,10 @@
-// Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
-const handler = async (event) => {
-  try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
-  } catch (error) {
-    return { statusCode: 500, body: error.toString() }
+exports.handler = async function(event, context) {
+  return {
+    statusCode: 200,
+    body: JSON.stringify({
+      name: 'Gillilo',
+      age: 34,
+      email: 'ghlwns1535@gmail.com'
+    })
   }
 }
-
-module.exports = { handler }
